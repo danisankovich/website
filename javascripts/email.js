8f0172976474355034692739dbeb365be9bb16a8
Michael Sankovich <misankovich@gmail.com>
2016-01-13 04:53:58 -0500

    emails. modals

--- javascripts/email.js
+++ javascripts/email.js
@@ -1,44 +1,56 @@
 jQuery(function($) {
-  $("#contact_form").submit(function() {
-    var email = $("#email").val();
-    var name = $("#name").val();
+  $('.sendMail').on("click", function() {
+    var email = $("#Email").val();
+    var name = $("#Name").val();
+    var subject = $("#Subject").val();
     var msg = $("#msg").val();
-    $.ajax({
-        type: "POST",
-        url: "https://mandrillapp.com/api/1.0/messages/send.json",
-        data: {
-          'key': 'MDTpzgc6BNZ7carbIFxuYw',
-          'message': {
-            'from_email': email,
-            'from_name': name,
-            'headers': {
-              'Reply-To': email
-            },
-            'subject': 'Hello Michael',
-            'text': msg,
-            'to': [{
-              'email': 'misankovich@gmail.com',
-              'name': 'Michael Sankovich',
-              'type': 'to'
-            }]
+    if(email && name && subject && msg) {
+      $.ajax({
+          type: "POST",
+          url: "https://mandrillapp.com/api/1.0/messages/send.json",
+          data: {
+            'key': 'MDTpzgc6BNZ7carbIFxuYw',
+            'message': {
+              'from_email': email,
+              'from_name': name,
+              'subject': subject,
+              'headers': {
+                'Reply-To': email
+              },
+              'text': msg,
+              'to': [{
+                'email': 'misankovich@gmail.com',
+                'name': 'Michael Sankovich',
+                'type': 'to'
+              }]
+            }
           }
-        }
-      })
-      .done(function(response) {
-        sweetAlert({title: "Your message has been sent.",
-                    text: "Thank you! I will be in touch as soon as possible.",
-                    type: "success"});
-        $("#name").val('');
-        $("#email").val('');
-        $("#msg").val('');
-      })
-      .fail(function(response) {
+        })
+        .done(function(response) {
+          sweetAlert({title: "Your message has been sent.",
+                      text: "Thank you! I will be in touch as soon as possible.",
+                      type: "success"});
+          $("#Name").val('');
+          $("#Email").val('');
+          $("#msg").val('');
+          $("#Subject").val('');
+          $('#emailModal').closeModal();
+        })
+        .fail(function(response) {
+          sweetAlert({
+  	         title: "Oops!",
+             text: "Something went wrong on the page!",
+             type: "error"
+           });
+        });
+      }
+      else {
         sweetAlert({
-	         title: "Oops!",
-           text: "Something went wrong on the page!",
+           title: "Oops!",
+           text: "All Fields Are Required",
            type: "error"
          });
-      });
+      }
     return false;
   });
 });
--- javascripts/script.js
+++ javascripts/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 
   $(".button-collapse").sideNav();
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+  var $nav = $("nav");
+
+	//caches a jQuery object containing the header element
+	// var header = $('.noBackground');
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 525) {
+      $nav.addClass("line");
+      $nav.removeClass("transparent");
+		} else {
+			$nav.removeClass("line");
+			$nav.addClass("transparent");
+		}
+	});
 
   //
   // $("#about").click(function() {
