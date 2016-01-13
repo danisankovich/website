1583fcf70c0c7dcd0d49d626a2d8e0691be6bd2d
Michael Sankovich <misankovich@gmail.com>
2016-01-12 23:56:41 -0500

    navbar

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,57 +1,61 @@
 $(document).ready(function(){
-  $("#about").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('.line').first().offset().top)
-    },500);
-  });
-  $("#aboutme").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('.line').first().offset().top)
-    },500);
-  });
-  $("#portfolio").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('#portstuff').first().offset().top)
-    },500);
-  });
-  $("#portfoliome").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('#portfolionav').first().offset().top)
-    },500);
-  });
-  $("#home").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('body').first().offset().top)
-    },500);
-  });
-  $("#homeme").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('body').first().offset().top)
-    },500);
-  });
-  $("#contacts").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('#contactme').first().offset().top)
-    },500);
-  });
-  $("#contactsme").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('#contactme').first().offset().top)
-    },500);
-  });
-  $("#emailmove").click(function() {
-    console.log('hello');
-    $('html, body').animate({
-      scrollTop: ($('#contactme').first().offset().top)
-    },500);
-  });
 
+  $(".button-collapse").sideNav();
+
+  //
+  // $("#about").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('.line').first().offset().top)
+  //   },500);
+  // });
+  // $("#aboutme").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('.line').first().offset().top)
+  //   },500);
+  // });
+  // $("#portfolio").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('#portstuff').first().offset().top)
+  //   },500);
+  // });
+  // $("#portfoliome").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('#portfolionav').first().offset().top)
+  //   },500);
+  // });
+  // $("#home").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('body').first().offset().top)
+  //   },500);
+  // });
+  // $("#homeme").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('body').first().offset().top)
+  //   },500);
+  // });
+  // $("#contacts").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('#contactme').first().offset().top)
+  //   },500);
+  // });
+  // $("#contactsme").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('#contactme').first().offset().top)
+  //   },500);
+  // });
+  // $("#emailmove").click(function() {
+  //   console.log('hello');
+  //   $('html, body').animate({
+  //     scrollTop: ($('#contactme').first().offset().top)
+  //   },500);
+  // });
+  //
 });
